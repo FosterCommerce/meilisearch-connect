90111b03c3a38e927654504ab37cccc4e4dd1a1a
Pete Eveleigh <pete@fostercommerce.com>
2026-04-09 10:53:19 +0100

    ecs fixes

--- src/services/Search.php
+++ src/services/Search.php
@@ -42,23 +42,29 @@ class Search extends Component
 	public function multisearch(array $indexHandles, string $query, array $searchParams = [], array $options = [], bool $federatedSearch = true): SearchResult
 	{
 		$indexes = Plugin::getInstance()->settings->getIndices($indexHandles, excludeSearchOnly: false);
-		$indexIds = array_map(fn($index) => $index->indexId, $indexes);
+		$indexIds = array_map(fn ($index) => $index->indexId, $indexes);
 
-		$searchable = array_map(fn($indexId) =>
+		$searchable = array_map(fn ($indexId) =>
 			(new SearchQuery())
 				->setIndexUid($indexId)
 				->setQuery($query), $indexIds);
 
-		if($federatedSearch === true) {
+		if ($federatedSearch === true) {
 			$federation = new MultiSearchFederation();
 			$federation->setLimit($searchParams['hitsPerPage'])->setOffset($searchParams['page'] - 1);
 
 			$result = $this->meiliClient->multiSearch($searchable, $federation);
-			return new SearchResult([...$result, 'query' => $query, 'page' => $searchParams['page']]);
-		} else {
-			$multiResult = $this->meiliClient->multiSearch($searchable);
-			$result = array_map(fn($resultsFromIndex) => (new SearchResult([...$resultsFromIndex, 'query' => $query])), $multiResult['results']);
-			dd(new SearchResult(...$multiResult['results']));
+			return new SearchResult([
+				...$result,
+				'query' => $query,
+				'page' => $searchParams['page'],
+			]);
 		}
+		$multiResult = $this->meiliClient->multiSearch($searchable);
+		$result = array_map(fn ($resultsFromIndex) => (new SearchResult([
+			...$resultsFromIndex,
+			'query' => $query,
+		])), $multiResult['results']);
+		dd(new SearchResult(...$multiResult['results']));
 	}
 }
